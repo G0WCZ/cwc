5165f793936cbb2e234c64cca1180447a121d425
Graeme Sutherland G0WCZ <grasuth@nodestone.io>
2019-07-22 22:49:56 +0100

    f-morseio-split # progress - add lang package, morse table and tree

--- go/cwc/lang/morse.go
+++ go/cwc/lang/morse.go
@@ -17,3 +17,129 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 package lang
+
+// Map of letters to morse - good for the encoder
+// and processed below to make a decoding tree
+var MorseMap = map[string]string{
+	"a": ".-",
+	"b": "-...",
+	"c": "-.-.",
+	"d": "-..",
+	"e": ".",
+	"f": "..-.",
+	"g": "--.",
+	"h": "....",
+	"i": "..",
+	"j": ".---",
+	"k": "-.-",
+	"l": ".-..",
+	"m": "--",
+	"n": "-.",
+	"o": "---",
+	"p": ".--.",
+	"q": "--.-",
+	"r": ".-.",
+	"s": "...",
+	"t": "-",
+	"u": "..-",
+	"v": "...-",
+	"w": ".--",
+	"x": "-..-",
+	"y": "-.--",
+	"z": "--..",
+	"1": ".----",
+	"2": "..---",
+	"3": "...--",
+	"4": "....-",
+	"5": ".....",
+	"6": "-....",
+	"7": "--...",
+	"8": "---..",
+	"9": "----.",
+	"0": "-----",
+	".": ".-.-.-",
+	",": "--..--",
+	"?": "..--..",
+	`'`: ".----.",
+	"/": "-..-.",
+	"(": "-.--.",
+	")": "-.--.-",
+	"&": ".-...",
+	":": "---...",
+	";": "-.-.-",
+	"=": "-...-",
+	"+": ".-.-.",
+	"-": "-....-",
+	"_": "..--.-",
+	`"`: ".-..-.",
+	"$": "...-..-",
+	"!": "-.-.--",
+	"@": ".--.-.",
+}
+
+type Symbol struct {
+	Text  string
+	Morse string
+	Dit   *Symbol
+	Dah   *Symbol
+}
+
+func MorseTree() *Symbol {
+	root := Symbol{"", "", nil, nil}
+	var last *Symbol = &root
+	var next *Symbol
+
+	for k, v := range MorseMap {
+		last = &root
+		next = nil
+
+		for i, b := range v {
+			if b == '.' {
+				next = last.Dit
+				if next == nil {
+					next = new(Symbol)
+					last.Dit = next
+				}
+			} else {
+				next = last.Dah
+				if next == nil {
+					next = new(Symbol)
+					last.Dah = next
+				}
+			}
+
+			if i+1 >= len(v) {
+				next.Text = k
+				next.Morse = v
+			} else {
+				last = next
+				next = nil
+			}
+		}
+	}
+
+	return &root
+}
+
+func SearchMorseTree(morse string, root *Symbol) string {
+	pos := root
+
+	for i, b := range morse {
+		if b == '.' {
+			pos = pos.Dit
+		} else {
+			pos = pos.Dah
+		}
+
+		if pos == nil {
+			return ""
+		}
+
+		if i+1 >= len(morse) {
+			return pos.Text
+		}
+
+	}
+
+	return ""
+}
