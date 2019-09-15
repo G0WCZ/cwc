e28b6bf440bf05e16be129e973c1a302f833333f
Graeme Sutherland G0WCZ <grasuth@nodestone.io>
2019-09-15 22:01:33 +0100

    f-morseio-split # register and parse inputs

--- go/core/hw/gpio_out.go
+++ go/core/hw/gpio_out.go
@@ -22,58 +22,45 @@ import (
 	"github.com/stianeikeland/go-rpio"
 )
 
-type GPIOIn struct {
+type GPIOOut struct {
 	Config      *config.Config
+	output      rpio.Pin
 	adapterName string
-	leftInput   rpio.Pin
-	rightInput  rpio.Pin
 }
 
-func NewGPIOIn(config *config.Config, adapterName string) MorseIn {
-	return &GPIOIn{Config: config, adapterName: adapterName}
-}
-
-func (g *GPIOIn) Open() error {
-	return g.initPorts(g.Config)
-}
-
-func (g *GPIOIn) ConfigChanged() error {
-	return g.initPorts(g.Config)
-}
-
-func (g *GPIOIn) initPorts(config *config.Config) error {
-	g.leftInput = rpio.Pin(config.GPIOPins.KeyLeft)
-	g.leftInput.Input()
-	g.leftInput.PullUp()
-
-	g.rightInput = rpio.Pin(config.GPIOPins.KeyRight)
-	g.rightInput.Input()
-	g.rightInput.PullUp()
-
-	return nil
-}
-
-func (g *GPIOIn) Close() error {
-	return nil
-}
-
-func (g *GPIOIn) Bit() bool {
-	return g.Dit()
-}
-
-// NB Active low - has a pullup. Keydown pulls to ground
-func (g *GPIOIn) Dit() bool {
-	if g.leftInput.Read() == rpio.High {
-		return false
-	} else {
-		return true
+func NewGPIOOut(config *config.Config, adapterName string) *GPIOOut {
+	gpio_out := GPIOOut{
+		Config:      config,
+		adapterName: adapterName,
 	}
+	return &gpio_out
 }
 
-func (g *GPIOIn) Dah() bool {
-	if g.rightInput.Read() == rpio.High {
-		return false
+func (g *GPIOOut) Open(config *config.Config) error {
+	g.Config = config
+	return g.initPorts(config)
+}
+
+func (g *GPIOOut) ConfigChanged() error {
+	return g.initPorts(g.Config)
+}
+
+func (g *GPIOOut) initPorts(config *config.Config) error {
+	g.output = rpio.Pin(config.GPIOPins.KeyOut)
+	g.output.Output()
+	g.output.Low()
+
+	return nil
+}
+
+func (g *GPIOOut) Close() error {
+	return nil
+}
+
+func (g *GPIOOut) SetBit(bit bool) {
+	if bit {
+		g.output.High()
 	} else {
-		return true
+		g.output.Low()
 	}
 }
